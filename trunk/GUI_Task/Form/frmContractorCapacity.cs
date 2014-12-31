ad63c18d2610db02f762109a9e47b068710e3de6
Qazii <bilalqazi360@gmail.com>
2014-12-31 23:07:46 +0500

    Qazi Commit

--- trunk/GUI_Task/Form/frmContractorCapacity.cs
+++ trunk/GUI_Task/Form/frmContractorCapacity.cs
@@ -6,11 +6,54 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using GUI_Task.Class;
 
 namespace GUI_Task
 {
     public partial class frmContractorCapacity : Form
     {
+        //******* Grid Variable Setting -- Begin ******
+        string fHDR = string.Empty;                       // Column Header
+        string fColWidth = string.Empty;                  // Column Width (Input)
+        string fColMinWidth = string.Empty;               // Column Minimum Width
+        string fColMaxInputLen = string.Empty;            // Column Visible Length/Width 
+        string fColFormat = string.Empty;                 // Column Format  
+        string fColReadOnly = string.Empty;               // Column ReadOnly 1 = ReadOnly, 0 = Read-Write  
+        string fFieldList = string.Empty;
+
+        string fColType = string.Empty;
+        string fFieldName = string.Empty;
+        //******* Grid Variable Setting -- End ******
+
+        string[] a_Color = new string[0];
+        int[] a_ColorInt = new int[0];
+
+        string[] a_Size = new string[0];
+        int[] a_SizeInt = new int[0];
+        List<string> fManySQL = null;                      // List string for storing Multiple Queri
+        string fRptTitle = string.Empty;
+        //bool fFormClosing = false;
+
+        bool ftTIsBalloon = true;
+        bool fEditMod = false;
+        int fEditRow = 0;
+        //bool fFrmLoading = true;                    // Form is Loading Controls (to accomodate Load event so that first time loading requirement is done)
+        int fTErr = 0;                              // Total Errors while Saving or other operation.
+        string ErrrMsg = string.Empty;              // To display error message if any.
+        string fLastID = string.Empty;              // Last Voucher/Doc ID (Saved new or modified)
+        //
+        //int fDocTypeID = 1;                         // Voucher/Doc Type ID
+        int fDocFiscal = 1;                         // Accounting / Fiscal Period    
+        //int fTNOA = 0;                              // Total Number of Attachments.    
+        int fTNOT = 0;                              // Total Number of Grid Transactions.
+        decimal fDocAmt = 0;                        // Document Amount Debit or Credit for DocMaster Field.
+        string fDocWhere = string.Empty;            // Where string to build where clause for Voucher level
+        int fLastRow = 0;                           // Last row number of the grid.
+        Int64 fDocID = 1;
+        bool fDocAlreadyExists = false;             // Check if Doc/voucher already exists (Edit Mode = True, New Mode = false)
+
+        bool blnFormLoad = true;
+        int fcboDefaultValue = 0;
         public frmContractorCapacity()
         {
             InitializeComponent();
@@ -18,12 +61,39 @@ namespace GUI_Task
 
         private void frm_Contractor_Capacity_Load(object sender, EventArgs e)
         {
+            this.MaximizeBox = false;
+            AtFormLoad();
+        }
 
+
+        private void AtFormLoad()
+        {
+            string lSQL = string.Empty;
+            //SettingGridVariable();
+            //LoadInitialControls();
+            this.KeyPreview = true;
+
+            //Item group cOMBO
+            lSQL = "select cgdCode, cgdDesc from catdtl where cgcode=" + Convert.ToString((int)Category.enmItemGroup);
+            lSQL += " order by cgdDesc";
+
+            clsFillCombo.FillCombo(cboItemGroup, clsGVar.ConString1, "CatDtl" + "," + "cgdCode" + "," + "False", lSQL);
+            fcboDefaultValue = Convert.ToInt16(cboItemGroup.SelectedValue);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void cboItemGroup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboItemGroup_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
